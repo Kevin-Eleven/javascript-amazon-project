807403d5e448089827be9f67c9e0e5b5972e5ce1
[Haris ] <harisahmad200411@gmail.com>
2024-06-06 16:21:37 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{
   productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 },{
@@ -6,6 +7,10 @@ export let cart = [{
   quantity: 1
 }];
 
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 export function addToCart(productId){
  
   let quant = document.querySelector(`.js-quantity-selector-${productId}`).value;
@@ -26,6 +31,8 @@ export function addToCart(productId){
       quantity : productQuantity
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -38,5 +45,6 @@ export function removeFromCart(productId){
 
   });
 
-  cart = newCart
+  cart = newCart;
+  saveToStorage();
 }
